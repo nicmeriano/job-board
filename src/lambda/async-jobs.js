1ce73dce6d27b3bc2158f0f3729eff575370dcb7
nicmeriano <nicmeriano@gmail.com>
2019-08-12 18:09:09 -0700

    sort jobs by date

--- src/lambda/async-jobs.js
+++ src/lambda/async-jobs.js
@@ -62,8 +62,38 @@ async function fetchGithub(searchTerm, location) {
     return true;
   });
 
+  // sort by date
+  const monthMap = {
+    Jan: "01",
+    Feb: "02",
+    Mar: "03",
+    Apr: "04",
+    May: "05",
+    Jun: "06",
+    Jul: "07",
+    Aug: "08",
+    Sep: "09",
+    Oct: "10",
+    Nov: "11",
+    Dec: "12"
+  };
+  const sortedJobs = jrJobs.sort((a, b) => {
+    let splitDateA = a.created_at.split(" ").splice(1);
+    let splitDateB = b.created_at.split(" ").splice(1);
+    let [monthA, dayA, , , yearA] = splitDateA;
+    let [monthB, dayB, , , yearB] = splitDateB;
+    let dateA = new Date(`${yearA}-${monthMap[monthA]}-${dayA}Z`);
+    let dateB = new Date(`${yearB}-${monthMap[monthB]}-${dayB}Z`);
+
+    if (dateA < dateB) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
   console.log(`got a total of ${jrJobs.length} jobs (FILTERED)`);
 
   console.log("fetching complete");
-  return jrJobs;
+  return sortedJobs;
 }
